868c449af97eb1bf49ff85f47604c764de242a02
wk645 <wk645@nyu.edu>
2017-08-28 10:35:49 -0400

    prop types

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,46 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+	render() {
+		return (
+			<div className="product">
+				<ul>
+					<li>{this.props.name}</li>
+					<li>{this.props.producer}</li>
+					<li>{this.props.hasWatermark ? 'true' : 'false'}</li>
+					<li>{this.props.color}</li>
+					<li>{this.props.weight}</li>
+
+				</ul>
+			</div>
+			)
+	}
+}
+
+Product.defaultProps = {
+	hasWatermark: false
+}
+
+Product.propTypes = {
+	name: PropTypes.string.isRequired,
+	producer: PropTypes.string,
+	hasWatermark: PropTypes.bool,
+	color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+	weight: function(props, propName) {
+		if (!props[propName]) {
+			return new Error(propName + ' is Required.')
+		}
+
+		if (typeof props[propName] !== 'number') {
+			return new Error(propName + 'is not a number.')
+		}
+
+		if ((props[propName] < 80 || props[propName] > 300)) { return new Error(propName + 'must be between 80 and 300.')
+		}
+	}
+}
+
+
+
